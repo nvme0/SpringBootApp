944e644934adbaee2fa04a708f904eb84de66ec7
nvme0 <jeffsummers@connect.qut.edu.au>
2020-02-28 16:05:54 +1000

    Makes TopicsController accept CORS requests

--- src/main/java/com/backend/app/api/topics/TopicsController.java
+++ src/main/java/com/backend/app/api/topics/TopicsController.java
@@ -3,12 +3,14 @@ package com.backend.app.api.topics;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class TopicsController {
 
